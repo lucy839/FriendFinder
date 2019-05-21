16179acefe1cd81a2ca67ab8834df725dc75be33
Youngju Lee <lucylee839@hotmail.com>
2019-05-20 20:39:40 -0700

    added comments to htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,15 @@
+// require path dependency
 var path = require('path');
 
+// route
 module.exports = function(app) {
+    //GET request 
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
+    // if no matching route is found default to home
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 };
     
-console.log('HTML Route Connected Successfully');
