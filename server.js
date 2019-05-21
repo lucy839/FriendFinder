6c9466952c64a27732e5116950f121ab9400d162
Youngju Lee <lucylee839@hotmail.com>
2019-05-20 20:20:20 -0700

    added comments to server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-// Dependencies
-// =============================================================
+// "dependencies" : set up dependencies
 var express = require("express");
 var path = require("path");
-// Sets up the Express App
-// =============================================================
+
+// "express configuration" : creates express server and sets up a port
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// static files for css
 app.use(express.static(path.join(__dirname, './app/public')));
 
-// Add the application routes
+// "router" : add the application routes
 require('./app/routing/apiRoutes.js')(app); 
 require('./app/routing/htmlRoutes.js')(app);
 
+// "listener" : effectively "starts" our server
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
